5c5230933a3a51774b8c758bdbb3c3738122a861
ramanromit <romit.rmn@gmail.com>
2025-03-04 19:00:01 +0530

    Add files via upload

---
+++ Stack_imp.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return "Stack is empty"
+
+    def top(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return "Stack is empty"
+
+    def is_empty(self):
+        return len(self.stack) == 0
+s = Stack()
+s.push(9)
+s.push(45)
+print(s.top())  
+s.pop()
+print(s.top())
